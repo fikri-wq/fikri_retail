7593b2ed806c2c68b677bb0801a9640b1f199f57
fikri-wq <tongwuan14@gmail.com>
2026-06-18 20:49:09 +0700

    build: rebuild web

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3612720771"
+    serviceWorkerVersion: "2456128925"
   }
 });
